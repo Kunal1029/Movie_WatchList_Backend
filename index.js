fe7da13fba60bc3e491d9600a511c6a68b534a91
kunal1029 <kunal.shivhare0529@gmail.com>
2024-07-06 12:34:11 +0530

    render

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const connectDB = require("./db");
 const movieRouter = require("./Routes/movieRoutes");
-
+const cors = require('cors');
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -12,6 +12,7 @@ const PORT = process.env.CLIENT_PORT || 2000;
 connectDB();
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use("/api/movie", movieRouter);
 
